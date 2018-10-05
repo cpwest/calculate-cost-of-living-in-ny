626c2e0373a3536c584c6dd510a71afa1593a3e9
Kalsoom Malik <kalsoomalik09@gmail.com>
2018-10-05 15:23:47 -0400

    Added Calculation for Rent Affordability

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,23 @@
 var searchButton = d3.select("#search-btn");
 
 
-// Event handlers for filter button
-searchButton.on("click", function () {
-});
+function updateRentStats() {
 
-
-function fetchProfessions() {
-    var url = "/professions";
-    d3.json(url).then(function (data) {
-        console.log("Data", data);
-        // Update drop down list
-    });
+    var div =  d3.select("#affordableRent");
+    div.innerHTML = "";
+    var innerDiv = div.append("div");
+    innerDiv.text(getAffordableRent(income));
 }
 
-function fetchWagesByProfession(profession) {
+updateRentStats()
 
-    var url = "/wages/" + profession;
-    d3.json(url).then(function (data) {
-        console.log("Data", data);
-        // perform calucations
-        // Dispay results
-    });
-}
+
+// function fetchWagesByProfession(profession) {
+//
+//     var url = "/wages/" + profession;
+//     d3.json(url).then(function (data) {
+//         console.log("Data", data);
+//         // perform calucations
+//         // Dispay results
+//     });
+// }
---
+++ static/js/calc.js
@@ -0,0 +1,11 @@
+const TAX_RATE_PERCENTAGE = 8;
+const RENT_AFFORDABLE_FACTOR = 0.3;
+
+// Method to calculate rent based on income
+function getAffordableRent(income) {
+
+    if (income === undefined || income === null) {
+        return 0;
+    }
+    return income * ((100 - TAX_RATE_PERCENTAGE) / 100) * RENT_AFFORDABLE_FACTOR;
+}
